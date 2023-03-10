7688d781a6b8f27422be997474538c883cb34df8
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-10 14:16:00 +0530

    new file:   script_seaborn.py

---
+++ script_seaborn.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+ 
+ 
+# reading the database
+data = pd.read_csv("tips.csv")
+ 
+sns.histplot(x='total_bill', data=data, kde=True, hue='sex')
+ 
+plt.show()
